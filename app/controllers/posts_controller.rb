2097e46e3e1e7951f3b752819d3aa1acd3e947f2
Javier Perez <perez.javi.a@gmail.com>
2016-10-23 15:53:03 -0400

    changes to navbar logo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :create, :delete]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
