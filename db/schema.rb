09e15bc816b898558d4e170e2bd02eead16b38eb
Javier Perez <perez.javi.a@gmail.com>
2016-10-24 19:50:43 -0400

    adding intro to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008172906) do
+ActiveRecord::Schema.define(version: 20161024235005) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161008172906) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "intro"
   end
 
   create_table "users", force: :cascade do |t|
