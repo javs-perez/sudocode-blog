9a8b4ca918bb2b7949c9f6a8a30f2330c8d014bc
Javier Perez <perez.javi.a@gmail.com>
2016-10-26 21:39:24 -0400

    adding slug column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024235005) do
+ActiveRecord::Schema.define(version: 20161027013812) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20161024235005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "intro"
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
