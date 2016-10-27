6220a18ae79c5b980621cff43bf8c754a7d75939
Javier Perez <perez.javi.a@gmail.com>
2016-10-26 20:01:01 -0400

    added a few styles and also a new logo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :intro)
   end
 
   def find_post
